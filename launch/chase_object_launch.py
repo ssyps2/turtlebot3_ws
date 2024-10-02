f8c152f20c4a98243a63f51be17044ea16606e2d
pengyuan <pengyuan.shu@outlook.com>
2024-10-01 21:53:12 -0400

    launch file added

--- launch/chase_object_launch.py
+++ launch/chase_object_launch.py
@@ -3,11 +3,11 @@ from launch_ros.actions import Node
 
 def generate_launch_description():
     return LaunchDescription([
-        # Node(
-        #     package='v4l2_camera',
-        #     executable='v4l2_camera_node',
-        #     parameters=['./v4l2_camera.yaml']
-        # ),
+        Node(
+            package='v4l2_camera',
+            executable='v4l2_camera_node',
+            parameters=['./v4l2_camera.yaml']
+        ),
         Node(
             package='object_tracker',
             executable='detect_object'
