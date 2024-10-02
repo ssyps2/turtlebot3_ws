7ff4272aa1715757e2944168404b72e8d7d9d940
pengyuan <pengyuan.shu@outlook.com>
2024-10-02 14:18:06 -0400

    move launch into package

--- src/object_tracker/setup.py
+++ src/object_tracker/setup.py
@@ -10,6 +10,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/launch', ['launch/chase_object.launch.py'])
     ],
     install_requires=['setuptools'],
     zip_safe=True,
